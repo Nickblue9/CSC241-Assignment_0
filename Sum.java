555df8b85cfe2d721e106244b6b21fa4a6628546
Nicholas Edwards <nicholas6598@gmail.com>
2019-08-26 17:12:43 -0400

    Fixed Tests

--- Sum.java
+++ Sum.java
@@ -3,12 +3,20 @@ import java.io.*;
 
 public class Sum{
     public static void main (String [] args){
-        String path;
+        Test_1("Test1.txt");
+        Test_2("Test2.txt");
+    }
+
+    public static void Test_1(String path){
+        System.out.println(getSum(path));
+    }
+
+    public static void Test_2(String path){
+        System.out.println(getSum(path));
+    }
+
+    private static int getSum(String path){
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter File Path: ");
-        path = input.nextLine();
-
         File f = new File(path);
         Scanner fScanner = null;
         try {
@@ -17,11 +25,11 @@ public class Sum{
 
         int sum = 0;
 
-            while (fScanner.hasNextLine()) {
-                try{
-                    sum+=Integer.parseInt(fScanner.nextLine());
-                }catch(Error e){e.printStackTrace();}
-            }
-        System.out.println("Sum:" +sum);
+        while (fScanner.hasNextLine()) {
+            try{
+                sum+=Integer.parseInt(fScanner.nextLine());
+            }catch(Error e){e.printStackTrace();}
+        }
+        return sum;
     }
 }
