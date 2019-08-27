b84209334bcfad0f28cb2f7b12b82dafbc963e1a
Nicholas Edwards <nicholas6598@gmail.com>
2019-08-27 09:50:41 -0400

    Uses args[0] for correct answer

--- Sum.java
+++ Sum.java
@@ -1,35 +1,21 @@
 import java.util.Scanner;
 import java.io.*;
 
-public class Sum{
-    public static void main (String [] args) throws FileNotFoundException{
-        Scanner input = new Scanner(System.in);
-        String path = input.nextLine();
-        test(path);
-    }
-
-    public static void test(String path) throws FileNotFoundException{
-        System.out.println(getSum(path));
-    }
-
-    public static void test() throws FileNotFoundException{
-        System.out.println("317");
-    }
-
-
-    private static int getSum(String path) throws FileNotFoundException{
-        Scanner input = new Scanner(System.in);
-        File f = new File(path);
-        Scanner fScanner = null;
-        fScanner = new Scanner(f);
+class Sum{
+    public static void main (String [] args){
+        File f = new File(args[0]);
 
         int sum = 0;
 
-        while (fScanner.hasNextLine()) {
-            try{
-                sum+=Integer.parseInt(fScanner.nextLine());
-            }catch(Error e){e.printStackTrace();}
+        try {
+            Scanner input = new Scanner(f);
+            while (input.hasNextLine()) {
+                sum += Integer.parseInt(input.nextLine());
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        return sum;
+        System.out.println(sum);
     }
 }
